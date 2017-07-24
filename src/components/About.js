ac4a0c31f98462c21f40154e173ed8cb6f0f7f24
Mark Mahoney <mrkmhny@users.noreply.github.com>
2017-07-23 22:06:11 -0400

    add padding to choice bars

--- src/components/About.js
+++ src/components/About.js
@@ -22,6 +22,14 @@ class About extends React.Component {
         <h2>I Am Human</h2>
         <p>Here's some more info about me.  I have a head and two arms and two legs and lots of toes and fingers</p>
         <p>I have eyeballs too</p>
+        <h2>I'm a Person</h2>
+        <p>Here's some more info about me.  I have a head and two arms and two legs and lots of toes and fingers</p>
+        <p>I have eyeballs too</p>
+        <p>Here's some more info about me.  I have a head and two arms and two legs and lots of toes and fingers</p>
+        <p>I have eyeballs too</p>
+        <h2>I Am Human</h2>
+        <p>Here's some more info about me.  I have a head and two arms and two legs and lots of toes and fingers</p>
+        <p>I have eyeballs too</p>
       </div>
     );
   }
