f86360fa7f3203e7dac082ef1e8b09da612825b2
Mark Mahoney <Mark@Marks-MacBook-Pro.local>
2017-07-27 01:18:12 -0400

    Build out projects section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,15 +9,59 @@ class Projects extends React.Component {
   }
 
   render() {
+
+    var projects = [
+      {
+        'name':'Simple Simon',
+        'link':'/about',
+        'about':'A simple simon game',
+        'languages':'Jquery, JS, html',
+        'icon':'sun-o'
+      },
+      {
+        'name':'Box-Crusher',
+        'link':'/about',
+        'about':'A simple simon game',
+        'languages':'Jquery, JS, html'
+      },
+      {
+        'name':'Instant Weather',
+        'link':'/about',
+        'about':'A simple simon game',
+        'languages':'Jquery, JS, html'
+      },
+      {
+        'name':'Simple Simon',
+        'link':'/about',
+        'about':'A simple simon game',
+        'languages':'Jquery, JS, html'
+      }
+    ]
+
+    var collection = Object.keys(projects).map(function(c,i,a){
+      return (
+        <div key={i} className="project">
+          <h2 className="project-name">{projects[i].name}</h2>
+          <div className="project-info">
+          <i className="fa fa-sun-o" aria-hidden="true"></i>
+            <div className="project-icon">
+              <i className="fa {'fa-'+projects[i].icon}" aria-hidden="true"></i>
+            </div>
+            <p className="project-about">{projects[i].about}</p>
+          </div>
+        </div>
+      );
+    })
+
     return (
 
       <div className="page projects">
         <h1>Projects</h1>
+
+
+
         <div className="collection">
-          <div className="project"></div>
-          <div className="project"></div>
-          <div className="project"></div>
-          <div className="project"></div>
+          {collection}
         </div>
       </div>
     );
